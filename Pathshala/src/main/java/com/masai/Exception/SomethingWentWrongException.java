59ce7d51911e0e2b02e6d9ea37ef2a21b1b4bbb1
chandan sharma <chandansharma695914@gmail.com>
2023-07-20 20:41:26 +0530

    done day2

---
+++ Pathshala/src/main/java/com/masai/Exception/SomethingWentWrongException.java
@@ -0,0 +1,9 @@
+package com.masai.Exception;
+
+
+public class SomethingWentWrongException extends Exception{
+    public SomethingWentWrongException(String message) {
+		// TODO Auto-generated constructor stub
+  	  super(message);
+	}
+}
